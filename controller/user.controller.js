f196f2ab03e0cfb5143eb739e7b4d7ec1f07930e
BHAVIK0709 <bhavikdpnachal7090@gmail.com>
2023-03-03 18:04:06 +0530

    3rd

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -13,9 +13,11 @@ const generateJWT = (user) => {
 
 const addUser = async (req, res) => {
   const { name, email, password } = req.body;
-  const existingUser = User.findOne({email:req.body.email})
-  if(existingUser){
-    return res.status(200).send({message:"User allready Exist",success:"false"})
+  const existingUser = User.findOne({ email: req.body.email });
+  if (existingUser) {
+    return res
+      .status(200)
+      .send({ message: "User allready Exist", success: "false" });
   }
   const hashedPassword = await bcrypt.hashSync(password, 10);
   await User.create({ name, email, password: hashedPassword })
@@ -46,12 +48,11 @@ const loginUser = async (req, res) => {
       if (isMatched) {
         const token = generateJWT(user);
         res.status(200).send({
-            success:true,
-            message: "Login Successfull",
-            _id: user._id,
-            email: user.email,
-            jwt: token,
-         
+          success: true,
+          message: "Login Successfull",
+          _id: user._id,
+          email: user.email,
+          jwt: token,
         });
       } else {
         res.status(403).send({
@@ -71,4 +72,32 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { addUser, getUser, loginUser };
+const authContoller = async (req, res) => {
+  try {
+    const authuser = await User.findOne({ _id: req.body._id });
+
+    if (!authuser) {
+      return res.status(200).send({
+        message: "user not found",
+        success: false,
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        data: {
+          name: authuser.name,
+          email: authuser.email,
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "auth error",
+      success: false,
+      error,
+    });
+  }
+};
+
+module.exports = { addUser, getUser, loginUser, authContoller };
--- router/user.router.js
+++ router/user.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRoutes =  express.Router();
-const {  addUser,getUser,loginUser } = require('../controller/user.controller.js');
+const {  addUser,getUser,loginUser, authContoller } = require('../controller/user.controller.js');
+const authenticateJWT = require('../middleware/auth.js');
 
 userRoutes.post('/signup',addUser );
 userRoutes.get('/getUsers',getUser);
 userRoutes.post('/login',loginUser);
+userRoutes.post('/getUserData' ,authenticateJWT,authContoller)
 
 module.exports = userRoutes;
