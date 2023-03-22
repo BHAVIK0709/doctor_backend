04097c400b3bce540309abc2fcd2bd65f1d3b3f1
BHAVIK0709 <bhavikdpnachal7090@gmail.com>
2023-03-22 18:15:03 +0530

    booking api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,9 @@
 const UserModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const appointmentModel = require("../models/appointmentModel");
+const userModel = require("../models/userModel");
+const moment = require("moment");
 
 const generateJWT = (user) => {
   const token = jwt.sign(
@@ -14,13 +17,14 @@ const generateJWT = (user) => {
 const addUser = async (req, res) => {
   const { name, email, password } = req.body;
   // const existingUser = UserModel.findOne({ email: req.body.email });
- 
+
   const hashedPassword = await bcrypt.hashSync(password, 10);
   await UserModel.create({ name, email, password: hashedPassword })
     .then((data) => {
       res.json(data).status(200);
     })
     .catch((err) => {
+      hbbbbbbbbbbb;
       res.json({ message: err.msg }).status(500);
     });
 };
@@ -47,13 +51,13 @@ const loginUser = async (req, res) => {
           success: true,
           message: "Login Successfull",
           _id: user._id,
-          name:user.name,
+          name: user.name,
           email: user.email,
           jwt: token,
-        isAdmin:user.isAdmin ,
-        notification:user.notification,
-        seennotification:user.seennotification,
-        isDoctor:user.isDoctor,
+          isAdmin: user.isAdmin,
+          notification: user.notification,
+          seennotification: user.seennotification,
+          isDoctor: user.isDoctor,
         });
       } else {
         res.status(403).send({
@@ -77,7 +81,7 @@ const authContoller = async (req, res) => {
   // console.log('wjueue',req.body)
   try {
     const authuser = await UserModel.findById({ _id: req.body.userId });
-// console.log(authuser)
+    // console.log(authuser)
     if (!authuser) {
       return res.status(200).send({
         message: "user not found",
@@ -87,7 +91,7 @@ const authContoller = async (req, res) => {
       res.status(200).send({
         success: true,
         data: {
-         authuser
+          authuser,
         },
       });
     }
@@ -101,4 +105,75 @@ const authContoller = async (req, res) => {
   }
 };
 
-module.exports = { addUser, getUser, loginUser, authContoller };
+const bookAppointmentCtrl = async (req, res) => {
+  try {
+    req.body.date = moment(req.body.date, "DD-MM-YYYY").toISOString();
+    req.body.time = moment(req.body.time, "HH:mm").toISOString();
+    req.body.status = "pending";
+    const newAppointment = await appointmentModel(req.body);
+    await newAppointment.save();
+
+    const user = await UserModel.find({ _id: req.body.userId });
+    const notification = user[0].notification;
+
+    console.log(notification);
+    notification.push({
+      type: `New Appointment Request from ${req.body.userInfo.name}`,
+      onClickPath: "/user/appointments",
+    });
+    console.log(notification);
+    await user[0].save();
+    res.status(200).send({
+      success: true,
+      message: "appointment booking successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "error whilw Booking appointment ",
+      success: false,
+      error,
+    });
+  }
+};
+
+const bookingAvailityCtrl = async (req, res) => {
+  try {
+    const date = moment(req.body.date, "DD-MM-YYYY").toISOString();
+    const fromTime= moment(req.body.time, "HH:mm").subtract(1,'hours').toISOString;
+    const toTime= moment(req.body.time, "HH:mm").subtract(1,'hours').toISOString;
+const doctorId = req.body.doctorId
+const appointments = await appointmentModel.find({doctorId,date,time:{
+$get : fromTime ,$let :toTime
+}})
+
+if(appointments.length > 0){
+  return res.status(200).send({
+    message:"appointment is not available on this time",
+    success:true
+  })
+}else{
+  return res.status(200).send({
+    success:true,
+    message:"Appointment is booked."
+  })
+}
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "error while fetching availibility of booking ",
+      success: false,
+      error,
+    });
+  }
+};
+
+module.exports = {
+  addUser,
+  getUser,
+  loginUser,
+  authContoller,
+  bookAppointmentCtrl,
+  bookingAvailityCtrl,
+};
--- router/user.router.js
+++ router/user.router.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const userRoutes =  express.Router();
-const {  addUser,getUser,loginUser, authContoller } = require('../controller/user.controller.js');
+const {  addUser,getUser,loginUser, authContoller, bookAppointmentCtrl, bookingAvailityCtrl } = require('../controller/user.controller.js');
 const authenticateJWT = require('../middleware/auth.js');
 
 userRoutes.post('/signup',addUser );
 userRoutes.get('/getUsers',getUser);
 userRoutes.post('/login',loginUser);
 userRoutes.get('/getUserData' ,authenticateJWT,authContoller)
+
+
+//Booking Appointment
+userRoutes.post('/book-apponitment',authenticateJWT, bookAppointmentCtrl);
+
+//Booking availibility
+userRoutes.post('/booking-availibility',authenticateJWT,bookingAvailityCtrl);
 
 module.exports = userRoutes;
